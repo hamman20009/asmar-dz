4625df71e54a07d896f7c8da1c5413506c80a6e3
hamman20009 <103840054+hamman20009@users.noreply.github.com>
2022-08-07 17:10:37 +0200

    Update products.js

--- products.js
+++ products.js
@@ -2,11 +2,11 @@ const products = [
 	{
 		id: 2,
 		logo:'img/logo.png',
-		image: 'img/1.jpg',
+		image: 'img/1.png',
 		
 		title: 'سيروم فيتوغان لتساقط العشر     '  ,
 		price1: 4000,
-		price: 3500,
+		price: 2900,
 		description: '  '  ,
 		phone:0554139163,
 	},
