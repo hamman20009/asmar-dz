c21662c3483af41e32a3acea96bb3c4ebb050a31
hamman20009 <103840054+hamman20009@users.noreply.github.com>
2022-10-20 01:34:21 +0100

    Add files via upload

---
+++ formation/products.js
@@ -0,0 +1,16 @@
+const products = [
+	{
+		id: 2,
+		logo:'img/logo.png',
+		image: 'img/products/12.png',
+		image2: 'img/products/produit.png',
+		
+		title: ' العلاج النهائي لتساقط و انبات الشعر    ',
+		price1: 5500,
+		price: 3000,
+		description: '  السيروم المكون من 7 زيوت الذي يقضي على الصلع و تساقط الشعر و الشيب المبكر '   ,
+		phone: 0,
+	},
+
+	
+];
