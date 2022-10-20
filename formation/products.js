1c4f04a0db237b28e8ce78afbac9868cf8e5ed1b
hamman20009 <103840054+hamman20009@users.noreply.github.com>
2022-10-20 01:46:14 +0100

    Update products.js

--- formation/products.js
+++ formation/products.js
@@ -2,7 +2,7 @@ const products = [
 	{
 		id: 2,
 		logo:'img/logo.png',
-		image: 'img/products/12.png',
+		image: 'img/products/coach.jpg',
 		image2: 'img/products/produit.png',
 		
 		title: ' دورة فن الإلقاء والخطابة بدون أوراق ⏩ ',
