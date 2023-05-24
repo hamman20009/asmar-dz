7472af1bff4841783150388228c63ea6f06fcea1
hamman20009 <103840054+hamman20009@users.noreply.github.com>
2023-05-24 22:01:52 +0100

    Update products.js

--- products.js
+++ products.js
@@ -2,16 +2,14 @@ const products = [
 	{
 		id: 2,
 		logo:'img/logo.png',
-		image: 'img/1.jpg',
 		image: 'img/12.png',
-		image2: 'img/produit.png'
+		image2: 'img/produit.png',
 		
-		
-		title: ' علاج تساقط الشعر و انباته من جديد      ',
-		price1: 4500,
-		price: 3800,
-		description: '  '   ,
-		phone: 0554139163,
+		title: ' العلاج النهائي لتساقط و انبات الشعر    ',
+		price1: 5500,
+		price: 3900,
+		description: '  السيروم المكون من 7 زيوت الذي يقضي على الصلع و تساقط الشعر و الشيب المبكر '   ,
+		phone: 0,
 	},
 
 	
